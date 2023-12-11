663b0c2459c55cdd5b4c5f3b6c151678553514ff
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2023-12-11 19:10:03 +0200

    baseurl

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import eslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/the-wild-oasis",
   plugins: [react(), eslint()],
 })
